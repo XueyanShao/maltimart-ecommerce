8a517812d65bddc8c2dc5949d47a45085aea888f
Xueyan Shao <xueyanshaoshirley@gmail.com>
2023-01-23 22:51:20 -0800

    homepage done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,6 +11,7 @@ import heroImg from "../assets/images/hero-img.png"
 
 import Services from "../services/Services";
 import ProductsList from '../components/UI/ProductsList';
+import Clock from '../components/UI/Clock';
 
 import counterImg from "../assets/images/counter-timer-img.png"
 
@@ -18,14 +19,20 @@ const Home = () => {
 
   const [trendingProducts,setTrendingProducts] = useState([])
   const [bestSalesProducts,setBestSalesProducts] = useState([])
+  const [mobileProducts,setMobileProducts] = useState([])
+  const [wirelessProducts,setWirelessProducts] = useState([])
 
   const year = new Date().getFullYear()
 
   useEffect(() => {
     const filteredTrendingProducts = products.filter(item=> item.category === "chair")
     const filteredBestSalesProducts = products.filter(item=> item.category === "sofa")
+    const filteredMobileProducts = products.filter(item=> item.category === "mobile")
+    const filteredWirelessProducts = products.filter(item=> item.category === "wireless")
     setTrendingProducts(filteredTrendingProducts)
     setBestSalesProducts(filteredBestSalesProducts)
+    setMobileProducts(filteredMobileProducts)
+    setWirelessProducts(filteredWirelessProducts)
   }, [])
   return (
     <Helmet title={'Home'}>
@@ -81,10 +88,31 @@ const Home = () => {
       <section className="timer__count">
         <Container>
           <Row>
-            <Col lg="6" md="6"></Col>
+            <Col lg="6" md="6">
+              <div className="clock__top-content">
+                <h4 className="text-white fs-6 mb-2">Limited Offers</h4>
+                <h3 className="text-white fs-5 mb-3">Quality Armchair</h3>
+              </div>
+              <Clock />
+              <motion.button whileTap={{scale:1.2}} className="buy__btn store__btn">
+                <Link to="/shop">Visit Store</Link>
+              </motion.button>
+            </Col>
             <Col lg="6" md="6" className="text-end">
               <img src={counterImg} alt="" />
             </Col>
+          </Row>
+        </Container>
+      </section>
+
+      <section className="new__arrivals">
+        <Container>
+          <Row>
+            <Col lg="12" className="text-center">
+              <h1 className="section__title">New Arrivals</h1>
+            </Col>
+            <ProductsList data={mobileProducts}/>
+            <ProductsList data={wirelessProducts}/>
           </Row>
         </Container>
       </section>
